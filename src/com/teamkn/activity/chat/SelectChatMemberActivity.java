efd69712d4d587ac9768a4db4243ce313fe252f3
fushang318 <fushang318@gmail.com>
2012-07-05 11:42:27 +0800

    增加创建对话串时的选人界面

--- src/com/teamkn/activity/chat/SelectChatMemberActivity.java
+++ src/com/teamkn/activity/chat/SelectChatMemberActivity.java
@@ -1,14 +1,79 @@
 package com.teamkn.activity.chat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
 
 import com.teamkn.R;
+import com.teamkn.Logic.HttpApi;
 import com.teamkn.base.activity.TeamknBaseActivity;
+import com.teamkn.base.task.TeamknAsyncTask;
+import com.teamkn.base.utils.BaseUtils;
+import com.teamkn.model.Contact;
+import com.teamkn.model.database.ContactDBHelper;
+import com.teamkn.widget.adapter.SelectChatMemberListAdapter;
 
 public class SelectChatMemberActivity extends TeamknBaseActivity {
+  private List<Integer> select_chat_member_ids;
+  private ListView select_chat_member_lv;
+  
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.select_chat_member);
+    select_chat_member_ids = new ArrayList<Integer>();
+    select_chat_member_lv = (ListView)findViewById(R.id.select_chat_member_lv);
+    load_select_chat_member_list();
+    
+  }
+  
+  private void load_select_chat_member_list() {
+    new TeamknAsyncTask<Void, Void, SelectChatMemberListAdapter>(this,"载入联系人") {
+
+      @Override
+      public SelectChatMemberListAdapter do_in_background(Void... params) throws Exception {
+        List<Contact> list = ContactDBHelper.applied_contacts(current_user().user_id);
+        SelectChatMemberListAdapter adapter = new SelectChatMemberListAdapter(SelectChatMemberActivity.this,select_chat_member_ids);
+        adapter.add_items(list);
+        return adapter;
+      }
+
+      @Override
+      public void on_success(final SelectChatMemberListAdapter adapter) {
+        select_chat_member_lv.setOnItemClickListener(new OnItemClickListener() {
+
+          @Override
+          public void onItemClick(AdapterView<?> arg0, View convertView, int arg2,
+              long arg3) {
+            adapter.select_item(convertView);
+          }
+        });
+        select_chat_member_lv.setAdapter(adapter);
+      }
+
+    }.execute();
+  }
+
+  public void click_submit_select_chat_member(View view){
+    new TeamknAsyncTask<Void,Void,Void>(this,"正在创建") {
+
+      @Override
+      public Void do_in_background(Void... params) throws Exception {
+        List<Integer> user_list = select_chat_member_ids;
+        // TODO
+        return null;
+      }
+
+      @Override
+      public void on_success(Void result) {
+      }
+      
+    }.execute();
   }
 }
---
+++ src/com/teamkn/widget/adapter/SelectChatMemberListAdapter.java
@@ -0,0 +1,85 @@
+package com.teamkn.widget.adapter;
+
+import java.io.ByteArrayInputStream;
+import java.util.List;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.teamkn.R;
+import com.teamkn.base.activity.TeamknBaseActivity;
+import com.teamkn.base.adapter.TeamknBaseAdapter;
+import com.teamkn.model.Contact;
+
+public class SelectChatMemberListAdapter extends TeamknBaseAdapter<Contact> {
+  private List<Integer> select_chat_member_ids;
+
+  public SelectChatMemberListAdapter(TeamknBaseActivity activity, List<Integer> select_chat_member_ids) {
+    super(activity);
+    this.select_chat_member_ids = select_chat_member_ids;
+  }
+
+  @Override
+  public View inflate_view() {
+    return inflate(R.layout.list_select_chat_member_item, null);
+  }
+
+  @Override
+  public BaseViewHolder build_view_holder(
+      View view) {
+    ViewHolder view_holder      = new ViewHolder();
+    
+    view_holder.select_chat_member_user_id_tv = (TextView)view.findViewById(R.id.select_chat_member_user_id_tv);
+    view_holder.select_chat_member_user_avatar_iv = (ImageView)view.findViewById(R.id.select_chat_member_user_avatar_iv);
+    view_holder.select_chat_member_user_name_tv = (TextView)view.findViewById(R.id.select_chat_member_user_name_tv);
+    view_holder.select_chat_member_select_cb = (CheckBox)view.findViewById(R.id.select_chat_member_select_cb);
+    
+    return view_holder;
+  }
+
+  @Override
+  public void fill_with_data(BaseViewHolder holder, Contact item, int position) {
+    ViewHolder view_holder = (ViewHolder) holder;
+    view_holder.select_chat_member_user_id_tv.setTag(item.contact_user_id);
+    view_holder.select_chat_member_user_name_tv.setText(item.contact_user_name);
+    if(item.contact_user_avatar != null){
+      Bitmap bitmap = BitmapFactory.decodeStream(new ByteArrayInputStream(item.contact_user_avatar));
+      Drawable drawable = new BitmapDrawable(bitmap);
+      view_holder.select_chat_member_user_avatar_iv.setBackgroundDrawable(drawable);
+    }else{
+      view_holder.select_chat_member_user_avatar_iv.setBackgroundResource(R.drawable.user_default_avatar_normal);
+    }
+    if(select_chat_member_ids.indexOf(item.contact_user_id) == -1){
+      view_holder.select_chat_member_select_cb.setChecked(false);
+    }else{
+      view_holder.select_chat_member_select_cb.setChecked(true);
+    }
+  }
+  
+  public void select_item(View convertView){
+    ViewHolder view_holder = (ViewHolder)convertView.getTag();
+    CheckBox cb = view_holder.select_chat_member_select_cb;
+    
+    Integer id = (Integer)view_holder.select_chat_member_user_id_tv.getTag();
+    
+    if(cb.isChecked()){
+      cb.setChecked(false);
+      select_chat_member_ids.remove(id);
+    }else{
+      cb.setChecked(true);
+      select_chat_member_ids.add(id);
+    }
+  }
+  
+  private class ViewHolder implements BaseViewHolder {
+    TextView select_chat_member_user_id_tv;
+    ImageView select_chat_member_user_avatar_iv;
+    TextView select_chat_member_user_name_tv;
+    CheckBox select_chat_member_select_cb;
+  }
+
+}
